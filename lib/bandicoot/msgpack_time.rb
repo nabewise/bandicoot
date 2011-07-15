db51157ea923742331405b66475eddbb98d39264
Ben Hughes <ben@pixelmachine.org>
2011-07-15 13:33:29 -0400

    bugfix

--- lib/bandicoot/msgpack_time.rb
+++ lib/bandicoot/msgpack_time.rb
@@ -1,7 +1,7 @@
 # As it turns out, serializing Time is something we should support for
 # this sort of application
 class Time
-  def to_msgpack
-    {"__type__" => "unixts", "ts" => self.to_i}.to_msgpack
+  def to_msgpack(out='')
+    {"__type__" => "unixts", "ts" => self.to_i}.to_msgpack(out)
   end
 end
